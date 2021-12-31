df5f9419b332a5517141b39c8551b1b2956514ef
nk1603 <85380205+nk1603@users.noreply.github.com>
2021-12-31 12:00:44 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "Chennai super Kings";
+const accessCode2 = "Ruthuraj Gaikwad ";
+const accessCode3 = "DHONI";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text(" P R U S E   I K G S N  I N H E C N A ", 100,50)
+    fill("lightblue")
+    text("HINT : WHO WON THIS YEAR IPL ", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("  U R T A H U R J  I W G A D K A ", 700,150)
+    fill("lightblue")
+    text("Hint: WHO WON 2021 ORANGE CAP IN IPL  ", 700,170)
+
+    fill("white")
+    textSize(15)
+    text(" H I D O N", 100,250)
+    fill("lightblue")
+    text("Hint: WHO IS THE BEST CAPTAIN IN IPL", 100,270)
+
+}
